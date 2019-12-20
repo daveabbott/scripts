b39fd9e66a2f36584e15fe34a01f06f7512d9448
Dave Abbott <dave.abbott@me.com>
2019-12-20 14:41:16 +1100

    Update pi-hole.sh and ubuntu-deadline.sh

---
+++ docker/pi-hole.sh
@@ -0,0 +1,7 @@
+# white list
+spclient.wg.spotify.com
+
+# black list
+
+# block list
+https://raw.githubusercontent.com/vincentkenny01/spotblock/master/spotify
--- docker/ubuntu-deadline.sh
+++ docker/ubuntu-deadline.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+apt-get update
+
+apt-get install ssl-dev
+
+ulimit -n 64000
+
 # Install Deadline
 /deadline/DeadlineRepository-*-linux-x64-installer.run
 
