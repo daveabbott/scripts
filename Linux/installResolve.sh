ebba4a59161493f3df30491c991349f57e4cedf1
davidabbott <davidabbott@double-rabbi>
2021-10-13 22:10:44 +1100

    Update installResolve.sh

--- Linux/installResolve.sh
+++ Linux/installResolve.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 REPO_PATH="/mnt/kabbalah/library/Software"
+ARCHIVE="$REPO_PATH/DaVinci_Resolve/DaVinci_Resolve_Studio*Linux.zip"
 
 cd /tmp
 
-ARCHIVE="$REPO_PATH/DaVinci_Resolve/DaVinci_Resolve_Studio*Linux.zip"
-unzip $ARCHIVE
+unzip -q $ARCHIVE
 
 RESOLVE="/tmp/DaVinci_Resolve_Studio_*.run"
 $RESOLVE -iy
