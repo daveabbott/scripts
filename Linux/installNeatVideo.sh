c86628ad1bd9fc02319f26ec9edbbe926e886dd8
David Abbott <davidabbott@double-rabbi>
2022-02-20 17:34:36 +1100

    Update installNeatVideo.sh

--- Linux/installNeatVideo.sh
+++ Linux/installNeatVideo.sh
@@ -2,23 +2,12 @@
 
 REPO_PATH="/mnt/kabbalah/library/Software"
 
-# get OS name
-if type lsb_release >/dev/null 2>&1 ; then
-   DISTRO=$(lsb_release -i -s)
-elif [ -e /etc/os-release ] ; then
-   DISTRO=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
-elif [ -e /etc/some-other-release-file ] ; then
-   DISTRO=$(ihavenfihowtohandleotherhypotheticalreleasefiles)
-fi
+cd /tmp
 
-DISTRO=$(printf '%s\n' "$DISTRO" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+tar -xvf $REPO_PATH/NeatVideo/NeatVideo*.tgz
 
-case "$DISTRO" in
-	centos*) ICON_PATH="/home/davidabbott/.local/share/applications" ;;
-	fedora*) ICON_PATH="/usr/share/applications" ;;
-esac
+NEAT="/tmp/NeatVideo*.run"
 
-# remove old nuke install
-$REPO_PATH/NeatVideo/NeatVideo*.run --mode console
+$NEAT --mode console
 
 exit 0
