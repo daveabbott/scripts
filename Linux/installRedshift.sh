c8a89de861751bf58a0dd564edf579c8996c9296
David Abbott <davidabbott@double-rabbi>
2022-02-20 17:34:27 +1100

    Update installRedshift.sh

--- Linux/installRedshift.sh
+++ Linux/installRedshift.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 REPO_PATH="/mnt/kabbalah/library/Software"
-REDSHIFT_INSTALLER_PATH="$REPO_PATH/Redshift/Current"
+REDSHIFT_INSTALLER_PATH="$REPO_PATH/Redshift"
 
 REDSHIFT_INSTALLER="$REDSHIFT_INSTALLER_PATH/redshift*.run"
 
 chmod +x $REDSHIFT_INSTALLER
 
 $REDSHIFT_INSTALLER --quiet
+
+/usr/redshift/bin/redshiftLicensingTool
 
 exit 0
