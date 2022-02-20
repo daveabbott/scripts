7814f89180ca6fa61087be7c40ceac910eddc977
David Abbott <davidabbott@double-rabbi>
2022-02-20 17:34:56 +1100

    Update installDeadline.sh

--- Linux/installDeadline.sh
+++ Linux/installDeadline.sh
@@ -3,6 +3,6 @@
 REPO_PATH="/mnt/kabbalah/library/Software"
 
 # run installer
-$REPO_PATH/Deadline/DeadlineClient*.run --mode unattended --licensemode LicenseFree --connectiontype Remote --proxyrootdir 192.168.69.20:2847 --slavestartup true --launcherdaemon false
+$REPO_PATH/Deadline/DeadlineClient*.run --mode unattended --licensemode LicenseFree --connectiontype Remote --proxyrootdir 192.168.69.20:2847 --slavestartup true --launcherdaemon true
 
 exit 0
